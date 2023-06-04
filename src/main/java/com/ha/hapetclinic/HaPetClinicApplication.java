54a36a66c08feaf23b119cc4e8e21c3d923678e9
hasiabewardana <hasi.prasanga@gmail.com>
2023-06-04 14:11:50 +0530

    Initialized the ha-pet-clinic project.

---
+++ src/main/java/com/ha/hapetclinic/HaPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.ha.hapetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ha/hapetclinic/HaPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ha.hapetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
